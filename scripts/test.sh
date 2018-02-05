89254f52fb5e49a3e58268a83b4d3a44cf4d1938
Martin Ponce <doc.brown.83@gmail.com>
2018-02-05 19:46:09 +0800

    Using minicover to enforce code coverage, threshold at 80%

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,11 +3,8 @@ source ./scripts/constants.sh
 
 set -e
 
-COVERALLS=./tools/coveralls.net.0.7.0/tools/csmacnz.Coveralls.exe
-
 # only execute dotnet test on test projects
 echo -e "\n****************\nTESTING SOLUTION\n****************\n"
-find . -name "*$TEST_SUFFIX" -exec dotnet test {} --configuration Release --no-build \;
-
-# echo -e "\n*****************\nCHECKING COVERAGE\n*****************\n"
-# mono $COVERALLS...
+for project in test/**/*.csproj; do
+    dotnet test $project --configuration Release --no-build;
+done
