2b340c24ea6b5ad667ac1e02cc1f138d7d1b6467
Martin Ponce <doc.brown.83@gmail.com>
2018-02-08 19:50:22 +0800

    Added persistence options to appsettings.json.

--- src/Bubbio.WebApi/Startup.cs
+++ src/Bubbio.WebApi/Startup.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Bubbio.Core;
+using Bubbio.Persist.Mongo;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -13,17 +15,36 @@ namespace Bubbio.WebApi
 {
     public class Startup
     {
+        private IConfiguration Configuration { get; set; }
+        private string _connectionString;
+        private string _schema;
+
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            InitializeConfiguration(configuration);
         }
 
-        public IConfiguration Configuration { get; }
+        private void InitializeConfiguration(IConfiguration configuration)
+        {
+            var connectionString = configuration["ConnectionString"];
+            var schema = configuration["Schema"];
+
+            if(string.IsNullOrWhiteSpace(connectionString))
+                throw new InvalidOperationException("ConnectionString required in appsettings.json");
+
+            if(string.IsNullOrWhiteSpace(schema))
+                throw new InvalidOperationException("Schema required in appsettings.json");
+
+            Configuration = configuration;
+            _connectionString = connectionString;
+            _schema = schema;
+        }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddTransient<IRepository>(s => new Repository(_connectionString, _schema));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
