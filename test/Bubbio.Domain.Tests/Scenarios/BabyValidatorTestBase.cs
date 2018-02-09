4997140f57b24829cdb75208987873b0cb08388a
Martin Ponce <doc.brown.83@gmail.com>
2018-02-09 20:06:21 +0800

    Restructured existing tests with base scenarios.

---
+++ test/Bubbio.Domain.Tests/Scenarios/BabyValidatorTestBase.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Bubbio.Core.Types;
+using Bubbio.Domain.Validation;
+using Xunit;
+
+namespace Bubbio.Domain.Tests.Scenarios
+{
+    public class BabyValidatorTestBase
+    {
+        private Baby _babyPreValidation;
+        private Baby _babyPostValidation;
+
+        protected string NamePreValidation { get; set; }
+        protected string NamePostValidation { get; set; }
+
+        protected void BabyIsValidated()
+        {
+            try
+            {
+                _babyPostValidation = _babyPreValidation.Validate();
+            }
+            catch (ArgumentException){}
+        }
+
+        protected void BabyWithFirstname(string name) =>
+            _babyPreValidation = new BabyBuilder()
+                .WithFirstname(name)
+                .Build();
+
+        protected void FirstnameIsFormatted(string expected) =>
+            Assert.Equal(expected, _babyPostValidation.Name.First);
+
+        protected void BabyWithMiddlename(string name) =>
+            _babyPreValidation = new BabyBuilder()
+                .WithMiddlename(name)
+                .Build();
+
+        protected void MiddlenameIsFormatted(string expected) =>
+            Assert.Equal(expected, _babyPostValidation.Name.Middle);
+
+        protected void BabyWithLastname(string name) =>
+            _babyPreValidation = new BabyBuilder()
+                .WithLastname(name)
+                .Build();
+
+        protected void LastnameIsFormatted(string expected) =>
+            Assert.Equal(expected, _babyPostValidation.Name.Last);
+
+        protected void BabyIsNotAccepted() =>
+            Assert.Null(_babyPostValidation);
+    }
+}
---
+++ test/Bubbio.WebApi.Tests/RepositoryControllerTests.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace Bubbio.WebApi.Tests
+{
+    public class RepositoryControllerTests
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
